1ff12b942267ffbc2e5b0cc8b83a3d76369c6a98
Norse-God <0norsegod0@gmail.com>
2026-07-03 11:33:15 +0300

    add ToolUse script
    this script: axe break only tree/pickaxe break only cobblestone

--- Assets/Scripts/ToolUse.cs
+++ Assets/Scripts/ToolUse.cs
@@ -4,49 +4,70 @@ public class ToolUse : MonoBehaviour
 {
     public GameObject axe;
     public GameObject pickaxe;
+
+    public GameObject player;
+
     private bool IsActiveAxe;
     private bool IsActivePickaxe;
+    private Player_Attaced playerAttack;
 
     private void Start()
     {
         IsActiveAxe = false;
         IsActivePickaxe = false;
-    }
 
+        axe.SetActive(false);
+        pickaxe.SetActive(false);
+
+        playerAttack = player.GetComponent<Player_Attaced>();
+        playerAttack.enabled = false;
+    }
 
     void Update()
     {
+  
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            if (IsActiveAxe)
-            {
-                axe.SetActive(!IsActiveAxe);
-                IsActiveAxe = !IsActiveAxe;
-            }
-            else 
-            {
-                axe.SetActive(!IsActiveAxe);
-                IsActiveAxe = !IsActiveAxe;
-                pickaxe.SetActive(false);
-            }
+            IsActiveAxe = !IsActiveAxe;
+            IsActivePickaxe = false; 
+
+            axe.SetActive(IsActiveAxe);
+            pickaxe.SetActive(false);
         }
+
         if (Input.GetKeyDown(KeyCode.X))
         {
-            if (IsActivePickaxe)
+            IsActivePickaxe = !IsActivePickaxe;
+            IsActiveAxe = false; 
+
+            pickaxe.SetActive(IsActivePickaxe);
+            axe.SetActive(false);
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+        RaycastHit hit;
+
+        bool canAttack = false;
+
+        if (Physics.Raycast(ray, out hit, 3f))
+        {
+            GameObject lookedObject = hit.collider.gameObject;
+            if (IsActiveAxe && lookedObject.CompareTag("Tree"))
             {
-                pickaxe.SetActive(!IsActivePickaxe);
-                IsActivePickaxe = !IsActivePickaxe;
+                canAttack = true;
             }
-            else
+            else if (IsActivePickaxe && lookedObject.CompareTag("Cobblestone"))
             {
-                pickaxe.SetActive(!IsActivePickaxe);
-                IsActivePickaxe = !IsActivePickaxe;
-                axe.SetActive(false);
+                canAttack = true;
             }
         }
+
+        if (playerAttack.enabled != canAttack)
+        {
+            playerAttack.enabled = canAttack;
+        }
     }
-
-
-
 }
+
+
 
